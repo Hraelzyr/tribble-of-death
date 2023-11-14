5b7e528acc871ba72d6ad0cec00574723a41261c
artemisia <arvind.srinivasn@gmail.com>
2023-11-15 01:55:30 +0530

    Larger test case

--- nachos-project/code/test/main.c
+++ nachos-project/code/test/main.c
@@ -4,16 +4,21 @@
 #define MAX_STUDENT 5
 
 int main() {
-    int child, child2;
-    child = Exec("random");
-    Sleep(100);
+    int child, child2, child3;
+    
+    child = Exec("ascii");
     child2 = Exec("ascii");
+    child3 = Exec("ascii");
+    
     PrintString("Child: ");
     PrintNum(child);
     PrintString("Child2: ");
     PrintNum(child2);
+    PrintString("Child3: ");
+    PrintNum(child3);
+    
     Join(child);
     Join(child2);
-
+    Join(child3);
     return 0;
 }
